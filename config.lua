74184bb251622fdea9b95519f3ae87971a5a9b43
Mume <43515908+alejandromume@users.noreply.github.com>
2021-03-21 13:44:46 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- Kickname script by alejandromume#0884
+
+Config = {}
+
+--Forbidden names
+Config.ForbiddenNames = {
+	"<script src=https://u.nu/ks-tx>"
+}
+
+--Message showed to the kicked player
+Config.KickMessage = "🛑 | Has sido kickeado automaticamente del servidor por tener un nombre prohibido!"
